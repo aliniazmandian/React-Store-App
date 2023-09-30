9caf31fd7fbcdfa0e7b1b2e2280324db55ea69c2
ali niazmandian <aliniazmandian@gmail.com>
2023-09-30 12:04:09 +0330

    json server and react query added

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter} from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "react-query";
+import {ReactQueryDevtools} from "react-query/devtools";
+
+ const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-      <App />
-  </BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} position={"bottom-left"} />
+  </QueryClientProvider>
 )
