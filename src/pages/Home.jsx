fce9110303dc4710fd4c2e2d920434901f000c84
ali niazmandian <aliniazmandian@gmail.com>
2023-09-30 14:03:03 +0330

    add to cart handler added redux install

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,12 @@ import {useRqProductFetcher} from "../Hooks/react-query/useRqProductFetcher.js";
 
 function Home() {
 
-const onSuccess = (data)=>{
-    console.log(data)
-}
 
- const  {isLoading,data,isError,error,refetch} =   useRqProductFetcher(0,onSuccess)
+ const  {isLoading,data,isError,error,refetch} =   useRqProductFetcher(0)
+
+     const addToCartHandler = (productId)=>{
+         console.log(productId)
+     }
 
 
     if (isLoading){
@@ -25,15 +26,24 @@ const onSuccess = (data)=>{
            <main>
                <section className={'grid grid-cols-16 gap-5 '}  >
                    {data?.data.map((product)=>{
-                       return <section key={product.id} >
-                           <section>
+                       return <section className={'mt-5'} key={product.id} >
+                           <section className={'w-full rounded-2xl overflow-hidden max-w-md bg-black '}>
                                <div>
                                    <img className={''} src={product.image} alt={product.name} />
                                </div>
-                               <div>
-                                   <p>{product.name}</p>
-                                   <p>{product.price}</p>
+                               <div className={'flex w-full'} >
+                                   <div className={'bg-blue-300 w-full pl-2'} >
+                                       <p>Name: {product.name}</p>
+                                       <p>Price: {product.price}$</p>
+                                   </div>
+                                   <button
+                                       className={'bg-blue-400 hover:bg-gray-700 active:bg-blue-50 '}
+                                       onClick={()=>addToCartHandler(product.id)}
+                                   >Add to cart
+                                   </button>
                                </div>
+
+
                            </section>
                               </section>
 
